2570d60471579d461070c72f494e0732f648ab9b
Jonas Andreasson <jonas.m.andreasson@gmail.com>
2025-02-01 16:01:57 +0000

    Bumped dependencie versions

--- client/client.js
+++ client/client.js
@@ -1,8 +1,8 @@
 const { connect } = require("@crusaider/leakbot-api");
 
 connect({
-  username: "jonas.m.andreasson@gmail.com",
-  password: "wCwR6C9zakgzERbFGr",
+  username: "user",
+  password: "password",
 }).then((client) => {
   client.listUserDevices().then(console.log);
 });
